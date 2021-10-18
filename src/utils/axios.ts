babd80774759ace8023288af0a9b708924cdd84b
thanhpq <phanquangthanh0217@gmail.com>
2021-10-18 09:44:54 +0700

    fix bug

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,8 @@
 // axios.js
+import { notification } from 'antd';
 import axios from 'axios';
+import { history } from 'umi';
+
 // Add a request interceptor
 axios.interceptors.request.use(
   (config) => {
@@ -24,9 +27,60 @@ axios.interceptors.response.use(
   (response) =>
     // Do something with response data
     response,
-  (error) =>
+  (error) => {
+    const {
+      response: {
+        data: {
+          detail: { message: msg, status },
+        },
+      },
+    } = error;
+    switch (status) {
+      case 400:
+        notification.error({ message: 'Bad request', description: msg || error.message });
+        break;
+
+      case 401:
+        notification.error({
+          message: 'Token quá hạn, vui lòng đăng nhập lại',
+          description: msg || error.message,
+        });
+        localStorage.removeItem('vaiTro');
+        localStorage.removeItem('token');
+        history.replace({
+          pathname: '/user/login',
+        });
+        break;
+
+      case 404:
+        notification.error({
+          message:
+            'Lỗi không tìm thấy dữ liệu, bạn hãy thử f5 refresh lại trình duyệt để cập nhật phiên bản mới nhất.',
+          description: msg || error.message,
+        });
+        break;
+
+      case 405:
+        notification.error({
+          message: 'Truy vấn không được phép',
+          description: msg || error.message,
+        });
+        break;
+
+      case 409:
+        notification.error({ message: 'Dữ liệu chưa đúng', description: msg || error.message });
+        break;
+
+      case 500:
+        notification.error({ description: 'Server gặp lỗi', message: msg || error.message });
+        break;
+
+      default:
+        break;
+    }
     // Do something with response error
-    Promise.reject(error),
+    Promise.reject(error);
+  },
 );
 
 export default axios;
