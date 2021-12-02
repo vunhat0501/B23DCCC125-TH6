aeaccb3364207c94b2eef6011dc80ce2a82b3fc7
thanhpq <phanquangthanh0217@gmail.com>
2021-12-02 11:15:12 +0700

    fix bug

--- src/pages/DichVuMotCuaV2/QuanLyBieuMau/components/Form.tsx
+++ src/pages/DichVuMotCuaV2/QuanLyBieuMau/components/Form.tsx
@@ -2,9 +2,17 @@ import { useModel } from 'umi';
 import FormBieuMau from './FormTaoBieuMau';
 import FormTaoQuyTrinh from './FormTaoQuyTrinh';
 import FormThongTinChung from './FormThongTinChung';
+import { useEffect } from 'react';
 
 const Form = () => {
   const { current } = useModel('dichvumotcuav2');
+  const { setRecord } = useModel('thanhtoan');
+
+  useEffect(() => {
+    return () => {
+      setRecord(undefined);
+    };
+  }, []);
 
   return (
     <>
