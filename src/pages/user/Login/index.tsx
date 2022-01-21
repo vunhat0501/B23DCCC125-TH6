c7115e5f2f3b692a88453de26ff127baf2a9aa17
thanhpq <phanquangthanh0217@gmail.com>
2022-01-21 16:05:12 +0700

    fix man login

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -4,11 +4,14 @@ import data from '@/utils/data';
 // import { getPhanNhom } from '@/utils/utils';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import ProForm, { ProFormText } from '@ant-design/pro-form';
-import { ConfigProvider, message, Tabs } from 'antd';
+import { Button, Col, ConfigProvider, Divider, message, Row, Tabs } from 'antd';
 import viVN from 'antd/lib/locale/vi_VN';
 import React, { useState } from 'react';
-import { FormattedMessage, history, useIntl, useModel } from 'umi';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
+import { FormattedMessage, history, Link, useIntl, useModel } from 'umi';
 import styles from './index.less';
+import logoGoogle from '@/assets/googleicon.png';
 
 const goto = () => {
   if (!history) return;
@@ -80,6 +83,35 @@ const Login: React.FC = () => {
     }
     setSubmitting(false);
   };
+
+  const responseFacebook = (response: any) => {
+    // this.props.dispatch({
+    //   type: 'login/loginFacebook',
+    //   payload: {
+    //     accessToken: response.accessToken,
+    //   },
+    // });
+    console.log('error', response);
+  };
+
+  const responseGoogle = (response: any) => {
+    // this.props.dispatch({
+    //   type: 'login/loginGoogle',
+    //   payload: {
+    //     accessToken: response.accessToken,
+    //   },
+    // });
+    console.log('error', response);
+  };
+
+  const responseGoogleFail = (response: any) => {
+    console.log('error', response);
+  };
+
+  const responseFacebookFail = (response: any) => {
+    console.log('error', response);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.image} />
@@ -106,12 +138,90 @@ const Login: React.FC = () => {
                     defaultMessage: 'submit',
                   }),
                 },
-                render: (_, dom) => dom.pop(), // ko hỉu
+
+                render: (_, dom) => {
+                  return (
+                    <>
+                      {dom.pop()}
+                      <div style={{ width: '100%', textAlign: 'center', marginTop: '15px' }}>
+                        <Link
+                          className={styles.link}
+                          style={{ cursor: 'poiter' }}
+                          to="/user/quenMatKhau"
+                        >
+                          <FormattedMessage id="pages.login.forgotPassword" />
+                        </Link>
+                      </div>
+                      <Divider style={{ margin: '13px 0px' }} />
+                      <div style={{ textAlign: 'center' }}>
+                        <Button
+                          type="primary"
+                          onClick={() => {
+                            history.push('/user/register');
+                          }}
+                          style={{
+                            height: 40,
+                            padding: '0px 15px',
+                            fontSize: 16,
+                            borderRadius: 4,
+                            // width: '100%',
+                          }}
+                        >
+                          Đăng ký tài khoản
+                        </Button>
+                      </div>
+                      <br />
+                      <Row>
+                        <Col style={{ marginTop: 8 }} xs={24} md={24}>
+                          <FacebookLogin
+                            appId="529724911787253"
+                            autoLoad={false}
+                            fields="name,email,picture"
+                            callback={responseFacebook}
+                            cssClass={styles.facebookButton}
+                            onFailure={responseFacebookFail}
+                            icon="fa fa-facebook fa-fw"
+                            textButton="Đăng nhập với facebook"
+                            size="small"
+                            disableMobileRedirect
+                          />
+                        </Col>
+
+                        <Col style={{ marginTop: 8 }} xs={24} md={24}>
+                          <GoogleLogin
+                            clientId="912230541378-6b2qhkl6g93bfc8nukkjq1qg2h9jognf.apps.googleusercontent.com"
+                            icon={false}
+                            // isSignedIn={true}
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogleFail}
+                            cookiePolicy="single_host_origin"
+                            render={(renderProps) => (
+                              <Button className={styles.googleButton} onClick={renderProps.onClick}>
+                                <img
+                                  style={{
+                                    height: '1rem',
+                                    margin: '0px 4px 3px -10px',
+                                    width: '1rem',
+                                  }}
+                                  src={logoGoogle}
+                                />
+                                Đăng nhập với Google
+                              </Button>
+                            )}
+                            className={styles.googleButton}
+                          />
+                        </Col>
+                      </Row>
+                    </>
+                  );
+                },
+
                 submitButtonProps: {
                   loading: submitting,
                   size: 'large',
                   style: {
                     width: '100%',
+                    borderRadius: 5,
                   },
                 },
               }}
@@ -127,20 +237,14 @@ const Login: React.FC = () => {
                     defaultMessage: 'tab',
                   })}
                 />
-                <Tabs.TabPane
-                  key="accountAdmin"
-                  tab={intl.formatMessage({
-                    id: 'pages.login.accountLoginAdmin.tab',
-                    defaultMessage: 'tab',
-                  })}
-                />
               </Tabs>
 
-              {(type === 'account' || type === 'accountAdmin') && (
+              {type === 'account' && (
                 <>
                   <ProFormText
                     name="login"
                     fieldProps={{
+                      style: { borderRadius: 5 },
                       size: 'large',
                       prefix: <UserOutlined className={styles.prefixIcon} />,
                     }}
@@ -164,6 +268,7 @@ const Login: React.FC = () => {
                   <ProFormText.Password
                     name="password"
                     fieldProps={{
+                      style: { borderRadius: 5 },
                       size: 'large',
                       prefix: <LockOutlined className={styles.prefixIcon} />,
                     }}
