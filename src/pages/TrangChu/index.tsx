0944db85de30e5e7ee01c1c225d8623b81367b17
Ngo Nhan <nv.bigbang@gmail.com>
2023-05-03 15:08:46 +0700

    Update subtitle

--- src/pages/TrangChu/index.tsx
+++ src/pages/TrangChu/index.tsx
@@ -6,7 +6,9 @@ const TrangChu = () => {
     <Card bodyStyle={{ height: '100%' }}>
       <div className="home-welcome">
         <h1 className="title">PHÂN HỆ QUẢN LÝ ĐÀO TẠO</h1>
-        <h2 className="sub-title">HỆ THỐNG ĐẠI HỌC SỐ - HỌC VIỆN PHỤ NỮ VIỆT NAM</h2>
+        <h2 className="sub-title">
+          HỆ THỐNG PHẦN MỀM CHỈ ĐẠO, ĐIỀU HÀNH - HỌC VIỆN PHỤ NỮ VIỆT NAM
+        </h2>
       </div>
     </Card>
   );
