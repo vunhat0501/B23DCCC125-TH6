6b485441a690df9608ca1711d5ee6f5e6791d987
Ngo Nhan <nv.bigbang@gmail.com>
2023-05-10 09:04:11 +0700

    SSO Login: Clear cache when login fail

--- src/pages/user/Login/KeycloakLogin.tsx
+++ src/pages/user/Login/KeycloakLogin.tsx
@@ -1,15 +1,31 @@
+import { DeleteOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 import { useAuth } from 'react-oidc-context';
 
 const LoginWithKeycloak = () => {
   const auth = useAuth();
 
+  const onClearCache = () => {
+    localStorage.clear();
+    sessionStorage.clear();
+    auth.removeUser();
+    window.location.reload();
+  };
+
   if (auth.isLoading) {
     return <div>Đang chuyển tới trang đăng nhập...</div>;
   }
 
   if (auth.error) {
-    return <div>Có lỗi xảy ra... {auth.error.message}</div>;
+    return (
+      <div>
+        Có lỗi xảy ra... <pre>{auth.error.message}</pre>
+        <br />
+        <Button icon={<DeleteOutlined />} onClick={onClearCache} type="link">
+          Xóa bộ nhớ đệm
+        </Button>
+      </div>
+    );
   }
 
   return (
