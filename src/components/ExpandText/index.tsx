22a410e2a14a4adeb472e84f18469cb3dfb8a018
Ngô Văn Nhận <vannhan.ngo@aisoft.com.vn>
2023-08-15 15:40:16 +0700

    Expand text: fix

--- src/components/ExpandText/index.tsx
+++ src/components/ExpandText/index.tsx
@@ -3,53 +3,53 @@ import React, { useEffect, useState } from 'react';
 import { type TooltipProps } from 'antd/lib/tooltip';
 
 interface EllipsisProps {
-  rows?: number;
-  expandable?: boolean;
-  suffix?: string;
-  symbol?: React.ReactNode;
-  onEllipsis?: (ellipsis: boolean) => void;
-  tooltip?: React.ReactNode | TooltipProps;
+	rows?: number;
+	expandable?: boolean;
+	suffix?: string;
+	symbol?: React.ReactNode;
+	onEllipsis?: (ellipsis: boolean) => void;
+	tooltip?: React.ReactNode | TooltipProps;
 }
 
 interface IProps {
-  children: any;
-  style?: React.CSSProperties;
-  ellipsis?: EllipsisProps;
+	children: any;
+	style?: React.CSSProperties;
+	ellipsis?: EllipsisProps;
 }
 
 const ExpandText = (props: IProps) => {
-  const [expand, setExpand] = useState<boolean>(false);
-  const [counter, setCounter] = useState<number>(0);
+	const [expand, setExpand] = useState<boolean>(false);
+	const [counter, setCounter] = useState<number>(0);
 
-  useEffect(() => {
-    setExpand(false);
-  }, [props.children]);
+	useEffect(() => {
+		setExpand(false);
+	}, [props.children]);
 
-  const typoExpand = () => {
-    setExpand(true);
-    setCounter(!expand ? counter + 0 : counter + 1);
-  };
-  const typoClose = () => {
-    setExpand(false);
-    setCounter(!expand ? counter + 0 : counter + 1);
-  };
+	const typoExpand = () => {
+		setExpand(true);
+		setCounter(!expand ? counter + 0 : counter + 1);
+	};
+	const typoClose = () => {
+		setExpand(false);
+		setCounter(!expand ? counter + 0 : counter + 1);
+	};
 
-  return (
-    <div key={counter}>
-      <Typography.Paragraph
-        style={{ ...props.style }}
-        ellipsis={{
-          rows: 3,
-          symbol: 'Xem tiếp',
-          expandable: true,
-          ...props.ellipsis,
-          onExpand: typoExpand,
-        }}
-      >
-        {props.children}
-      </Typography.Paragraph>
-      {expand && <a onClick={typoClose}>Ẩn bớt</a>}
-    </div>
-  );
+	return (
+		<div key={counter}>
+			<Typography.Paragraph
+				style={{ ...props.style, overflowWrap: props.style?.overflowWrap ?? 'anywhere' }}
+				ellipsis={{
+					rows: 3,
+					symbol: 'Xem tiếp',
+					expandable: true,
+					...props.ellipsis,
+					onExpand: typoExpand,
+				}}
+			>
+				{props.children}
+			</Typography.Paragraph>
+			{expand && <a onClick={typoClose}>Ẩn bớt</a>}
+		</div>
+	);
 };
 export default ExpandText;
