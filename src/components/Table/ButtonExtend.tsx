61b2d9d1ac7969674692af6e58e0d88ef9d642fc
Ngô Văn Nhận <vannhan.ngo@aisoft.com.vn>
2023-08-18 14:44:10 +0700

    Button Extend: Fix

--- src/components/Table/ButtonExtend.tsx
+++ src/components/Table/ButtonExtend.tsx
@@ -1,25 +1,19 @@
-import { Button, Tooltip } from 'antd';
+import { Button, Tooltip, type ButtonProps } from 'antd';
 import React from 'react';
 
-const ButtonExtend = (props: {
-	icon: React.ReactNode;
-	children?: React.ReactNode;
-	tooltip?: React.ReactNode;
-	onClick?: () => void;
-	disabled?: boolean;
-	loading?: boolean;
-	size?: 'middle' | 'small';
-	type?: 'primary' | 'default';
-	notHideText?: boolean;
-	style?: React.CSSProperties;
-}) => {
-	const { icon, children, onClick, disabled, loading, size, type, tooltip, notHideText, style } = props;
+/** Button extend text with default Tooltip */
+const ButtonExtend = (
+	props: {
+		children?: React.ReactNode;
+		tooltip?: React.ReactNode;
+		notHideText?: boolean;
+	} & ButtonProps,
+) => {
+	const { children, tooltip, notHideText } = props;
 
 	return (
 		<Tooltip title={tooltip ?? children}>
-			<Button icon={icon} onClick={onClick} disabled={disabled} loading={loading} size={size} type={type} style={style}>
-				{!notHideText ? <span className='extend'>{children}</span> : children}
-			</Button>
+			<Button {...props}>{!notHideText ? <span className='extend'>{children}</span> : children}</Button>
 		</Tooltip>
 	);
 };
