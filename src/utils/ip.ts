3168651f28012738c152e75b213030ae0eceac72
Ngô Văn Nhận <vannhan.ngo@aisoft.com.vn>
2023-09-20 14:30:30 +0700

    Avatar Dropdown: update name

--- src/utils/ip.ts
+++ src/utils/ip.ts
@@ -15,6 +15,7 @@ const keycloakAuthority = ipRoot + 'keycloak/realms/vwa';
 const resourceServerClientId = 'vwa-auth';
 const keycloakTokenEndpoint = ipRoot + 'keycloak/realms/vwa/protocol/openid-connect/token';
 const sentryDSN = 'https://ed934e521d476c44a89a42aaa8a6993a@sentry.aisoftech.vn/3';
+const oneSignalClient = 'f3857a81-2891-49be-87a7-903a4a1a54be';
 
 export {
 	ip3,
@@ -26,4 +27,5 @@ export {
 	keycloakTokenEndpoint,
 	keycloakAuthority,
 	sentryDSN,
+	oneSignalClient,
 };
