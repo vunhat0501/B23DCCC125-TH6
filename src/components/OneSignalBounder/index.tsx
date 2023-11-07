a499ba3897ed3c3d81c95a146943095083616f96
Ngô Văn Nhận <vannhan.ngo@aisoft.com.vn>
2023-11-07 15:27:33 +0700

    One Signal: Fix

--- src/components/OneSignalBounder/index.tsx
+++ src/components/OneSignalBounder/index.tsx
@@ -1,7 +1,6 @@
 import { initOneSignal } from '@/services/base/api';
 import { AppModules } from '@/services/base/constant';
 import { currentRole, oneSignalClient, oneSignalRole } from '@/utils/ip';
-import queryString from 'query-string';
 import { useEffect, useState } from 'react';
 import { useAuth } from 'react-oidc-context';
 import OneSignal from 'react-onesignal';
@@ -18,15 +17,6 @@ const OneSignalBounder = (props: { children: React.ReactNode }) => {
 		});
 		const id = await OneSignal.getUserId();
 		setOneSignalId(id);
-	};
-
-	/** Gửi message đến trang chính sau khi handle OneSignal */
-	const sendMessage = (isEnable: boolean) => {
-		const parsed = queryString.parse(window.location.search);
-		// console.log(`2 Mainsite is Sending Message to ${parsed?.source?.toString() ?? ''} ${isEnable}`);
-		// postMessage: https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
-		// && Object.values(AppModules).some((role) => role.url === parsed.source)
-		if (parsed.source) window.parent.postMessage(isEnable ? oneSignalId : false, parsed.source.toString());
 	};
 
 	/** Show Popup center screen */
@@ -87,12 +77,10 @@ const OneSignalBounder = (props: { children: React.ReactNode }) => {
 
 	/**
 	 * Init OneSignal playerId with auth User
-	 * and Send message to request domain
 	 */
 	useEffect(() => {
 		if (oneSignalId) {
 			if (auth.user?.access_token) initOneSignal({ playerId: oneSignalId });
-			OneSignal.isPushNotificationsEnabled(sendMessage);
 		}
 	}, [oneSignalId, auth.user?.access_token]);
 
--- src/pages/ThongBao/Check.tsx
+++ src/pages/ThongBao/Check.tsx
@@ -1,16 +1,27 @@
-import { useEffect, useState } from 'react';
-import OneSignalReact from 'react-onesignal';
+import queryString from 'query-string';
+import { useEffect } from 'react';
+import OneSignal from 'react-onesignal';
 
 const CheckOneSignalSubscription = () => {
-	const [oneSignalId, setOneSignalId] = useState<string | null | undefined>();
+	/** Gửi message đến trang chính sau khi handle OneSignal */
+	const sendMessage = async (isEnable: boolean) => {
+		const parsed = queryString.parse(window.location.search);
+		// console.log(`2 Mainsite is Sending Message to ${parsed?.source?.toString() ?? ''} ${isEnable}`);
+		// postMessage: https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
+		// && Object.values(AppModules).some((role) => role.url === parsed.source)
+		if (parsed.source) window.parent.postMessage(isEnable, parsed.source.toString());
+	};
 
 	useEffect(() => {
-		OneSignalReact.getUserId().then((id) => {
-			setOneSignalId(id);
+		window.addEventListener('load', () => {
+			OneSignal.isPushNotificationsEnabled((isEnabled) => {
+				// console.log(`1 subdomain.site iframe checking subscription from mainsite, it is ${isEnabled}`);
+				sendMessage(isEnabled);
+			});
 		});
 	}, []);
 
-	return <>Hello {oneSignalId}</>;
+	return <>Hello </>;
 };
 
 export default CheckOneSignalSubscription;
