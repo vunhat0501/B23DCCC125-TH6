39cc03333afa5023f0a7222895fd89212dda28f0
Ngô Văn Nhận <vannhan.ngo@aisoft.com.vn>
2023-12-28 09:45:24 +0700

    Update: Hệ thống chuyển đổi số

--- src/pages/TienIch/GioiThieu/index.tsx
+++ src/pages/TienIch/GioiThieu/index.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
 		<Card bodyStyle={{ height: '100%' }}>
 			<div className='home-welcome'>
 				<h1 className='title'>GIỚI THIỆU {unitName.toUpperCase()}</h1>
-				<h2 className='sub-title'>HỆ THỐNG PHẦN MỀM CHỈ ĐẠO, ĐIỀU HÀNH - {unitName.toUpperCase()}</h2>
+				<h2 className='sub-title'>HỆ THỐNG CHUYỂN ĐỔI SỐ - {unitName.toUpperCase()}</h2>
 			</div>
 		</Card>
 	);
--- src/pages/TrangChu/index.tsx
+++ src/pages/TrangChu/index.tsx
@@ -7,7 +7,7 @@ const TrangChu = () => {
 		<Card bodyStyle={{ height: '100%' }}>
 			<div className='home-welcome'>
 				<h1 className='title'>PHÂN HỆ QUẢN LÝ ĐÀO TẠO</h1>
-				<h2 className='sub-title'>HỆ THỐNG PHẦN MỀM CHỈ ĐẠO, ĐIỀU HÀNH - {unitName.toUpperCase()}</h2>
+				<h2 className='sub-title'>HỆ THỐNG CHUYỂN ĐỔI SỐ - {unitName.toUpperCase()}</h2>
 			</div>
 		</Card>
 	);
