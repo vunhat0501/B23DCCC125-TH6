62f6d0d410832fbfe386ee885e01eca7fe3b73f8
Ngô Văn Nhận <vannhan.ngo@aisoft.com.vn>
2024-11-22 09:31:22 +0700

    update check access

--- src/hooks/useCheckAccess.tsx
+++ src/hooks/useCheckAccess.tsx
@@ -1,9 +1,8 @@
-import { currentRole } from '@/utils/ip';
 import { useModel } from 'umi';
 
 const useCheckAccess = (code: string): boolean => {
 	const { initialState } = useModel('@@initialState');
-	const scopes = initialState?.authorizedPermissions?.find((item) => item.rsname === currentRole)?.scopes;
+	const scopes = initialState?.authorizedPermissions?.flatMap((item) => item.scopes);
 
 	return scopes?.includes(code) || false;
 };
