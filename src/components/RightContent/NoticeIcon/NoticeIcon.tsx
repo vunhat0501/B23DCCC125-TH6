6d0e7a9c16aca9cd1c0d6a42568b209598e71bac
Ngô Văn Nhận <vannhan.ngo@aisoft.com.vn>
2024-12-25 13:56:03 +0700

    Thông báo: Fix read noti

--- src/components/RightContent/NoticeIcon/NoticeIcon.tsx
+++ src/components/RightContent/NoticeIcon/NoticeIcon.tsx
@@ -1,3 +1,4 @@
+import { type ThongBao } from '@/services/ThongBao/typing';
 import { BellOutlined } from '@ant-design/icons';
 import { Badge, Spin, Tabs, Tooltip } from 'antd';
 import useMergedState from 'rc-util/es/hooks/useMergedState';
@@ -6,117 +7,104 @@ import HeaderDropdown from '../HeaderDropdown';
 import type { NoticeIconTabProps } from './NoticeList';
 import NoticeList from './NoticeList';
 import styles from './index.less';
-import { type ThongBao } from '@/services/ThongBao/typing';
 const { TabPane } = Tabs;
 
 export type NoticeIconProps = {
-  count?: number;
-  bell?: React.ReactNode;
-  className?: string;
-  loading?: boolean;
-  onClear?: (tabName: string, tabKey: string) => void;
-  onItemClick?: (item: ThongBao.IRecord, tabProps: NoticeIconTabProps) => void;
-  onViewMore?: (tabProps: NoticeIconTabProps) => void;
-  onTabChange?: (tabTile: string) => void;
-  style?: React.CSSProperties;
-  onPopupVisibleChange?: (visible: boolean) => void;
-  popupVisible?: boolean;
-  clearText?: string;
-  viewMoreText?: string;
-  clearClose?: boolean;
-  emptyImage?: string;
-  children?: React.ReactElement<NoticeIconTabProps>;
+	count?: number;
+	bell?: React.ReactNode;
+	className?: string;
+	loading?: boolean;
+	onClear?: (tabName: string, tabKey: string) => void;
+	onItemClick?: (item: ThongBao.IRecord, tabProps: NoticeIconTabProps) => void;
+	onViewMore?: (tabProps: NoticeIconTabProps) => void;
+	onTabChange?: (tabTile: string) => void;
+	style?: React.CSSProperties;
+	onPopupVisibleChange?: (visible: boolean) => void;
+	popupVisible?: boolean;
+	clearText?: string;
+	viewMoreText?: string;
+	clearClose?: boolean;
+	emptyImage?: string;
+	children?: React.ReactElement<NoticeIconTabProps>;
 };
 
 const NoticeIcon: React.FC<NoticeIconProps> & {
-  Tab: typeof NoticeList;
+	Tab: typeof NoticeList;
 } = (props) => {
-  const getNotificationBox = (): React.ReactNode => {
-    const {
-      children,
-      loading,
-      onClear,
-      onTabChange,
-      onItemClick,
-      onViewMore,
-      clearText,
-      viewMoreText,
-    } = props;
-    if (!children) {
-      return null;
-    }
-    const panes: React.ReactNode[] = [];
-    React.Children.forEach(children, (child: React.ReactElement<NoticeIconTabProps>): void => {
-      if (!child) {
-        return;
-      }
-      const { list, title, count, tabKey, showClear, showViewMore } = child.props;
-      const len = list && list.length ? list.length : 0;
-      const msgCount = count || count === 0 ? count : len;
-      const tabTitle: string = msgCount > 0 ? `${title} (${msgCount})` : title;
-      panes.push(
-        <TabPane closable={false} tab={tabTitle} key={tabKey}>
-          <NoticeList
-            clearText={clearText}
-            viewMoreText={viewMoreText}
-            list={list}
-            tabKey={tabKey}
-            onClear={() => onClear && onClear(title, tabKey)}
-            onClick={(item) => onItemClick && onItemClick(item, child.props)}
-            onViewMore={() => onViewMore && onViewMore(child.props)}
-            showClear={showClear}
-            showViewMore={showViewMore}
-            title={title}
-          />
-        </TabPane>,
-      );
-    });
-    return (
-      <>
-        <Spin spinning={loading} delay={300}>
-          <Tabs className={styles.tabs} onChange={onTabChange}>
-            {panes}
-          </Tabs>
-        </Spin>
-      </>
-    );
-  };
+	const getNotificationBox = (): React.ReactNode => {
+		const { children, loading, onClear, onTabChange, onItemClick, onViewMore, clearText, viewMoreText } = props;
+		if (!children) {
+			return null;
+		}
+		const panes: React.ReactNode[] = [];
+		React.Children.forEach(children, (child: React.ReactElement<NoticeIconTabProps>): void => {
+			if (!child) {
+				return;
+			}
+			const { list, title, count, tabKey, showClear, showViewMore } = child.props;
+			const len = list && list.length ? list.length : 0;
+			const msgCount = count || count === 0 ? count : len;
+			const tabTitle: string = msgCount > 0 ? `${title} (${msgCount})` : title;
+			panes.push(
+				<TabPane closable={false} tab={tabTitle} key={tabKey}>
+					<NoticeList
+						clearText={clearText}
+						viewMoreText={viewMoreText}
+						list={list}
+						tabKey={tabKey}
+						onClear={() => onClear && onClear(title, tabKey)}
+						onClick={(item) => onItemClick && onItemClick(item, child.props)}
+						onViewMore={() => onViewMore && onViewMore(child.props)}
+						showClear={showClear}
+						showViewMore={showViewMore}
+						title={title}
+					/>
+				</TabPane>,
+			);
+		});
+		return (
+			<>
+				<Spin spinning={loading} delay={300}>
+					<Tabs className={styles.tabs} onChange={onTabChange}>
+						{panes}
+					</Tabs>
+				</Spin>
+			</>
+		);
+	};
 
-  const { count, bell } = props;
-  const [visible, setVisible] = useMergedState<boolean>(false, {
-    value: props.popupVisible,
-    onChange: props.onPopupVisibleChange,
-  });
+	const { count, bell } = props;
+	const [visible, setVisible] = useMergedState<boolean>(false, {
+		value: props.popupVisible,
+		onChange: props.onPopupVisibleChange,
+	});
 
-  const notificationBox = getNotificationBox();
-  if (!notificationBox) return <></>;
+	const notificationBox = getNotificationBox();
+	if (!notificationBox) return <></>;
 
-  return (
-    <HeaderDropdown
-      placement="bottomRight"
-      overlay={notificationBox}
-      overlayClassName={styles.popover}
-      trigger={['click']}
-      visible={visible}
-      onVisibleChange={setVisible}
-      arrow
-    >
-      <Tooltip title="Thông báo" placement="bottom">
-        <a className={styles.badge}>
-          <Badge
-            count={count ? (count < 100 ? count : '99+') : undefined}
-            style={{ boxShadow: 'none' }}
-          >
-            {bell || <BellOutlined />}
-          </Badge>
-        </a>
-      </Tooltip>
-    </HeaderDropdown>
-  );
+	return (
+		<HeaderDropdown
+			placement='bottomRight'
+			overlay={notificationBox}
+			overlayClassName={styles.popover}
+			trigger={['click']}
+			visible={visible}
+			onVisibleChange={setVisible}
+			arrow
+		>
+			<Tooltip title='Thông báo' placement='bottom'>
+				<a className={styles.badge}>
+					<Badge count={count ? (count < 100 ? count : '99+') : undefined} style={{ boxShadow: 'none' }}>
+						{bell || <BellOutlined />}
+					</Badge>
+				</a>
+			</Tooltip>
+		</HeaderDropdown>
+	);
 };
 
 NoticeIcon.defaultProps = {
-  emptyImage: 'https://gw.alipayobjects.com/zos/rmsportal/wAhyIChODzsoKIOBHcBk.svg',
+	emptyImage: 'https://gw.alipayobjects.com/zos/rmsportal/wAhyIChODzsoKIOBHcBk.svg',
 };
 
 NoticeIcon.Tab = NoticeList;
--- src/components/RightContent/NoticeIcon/index.tsx
+++ src/components/RightContent/NoticeIcon/index.tsx
@@ -5,85 +5,84 @@ import { useModel } from 'umi';
 import NoticeIcon from './NoticeIcon';
 
 const NoticeIconView = () => {
-  const {
-    danhSach,
-    getThongBaoModel,
-    total,
-    page,
-    limit,
-    setLimit,
-    loading,
-    record,
-    setRecord,
-    unread,
-    readNotificationModel,
-  } = useModel('thongbao.noticeicon');
-  const [visibleDetail, setVisibleDetail] = useState<boolean>(false);
-  const [visiblePopup, setVisiblePopup] = useState<boolean>(false);
+	const {
+		danhSach,
+		getThongBaoModel,
+		total,
+		page,
+		limit,
+		setLimit,
+		loading,
+		record,
+		setRecord,
+		unread,
+		readNotificationModel,
+	} = useModel('thongbao.noticeicon');
+	const [visibleDetail, setVisibleDetail] = useState<boolean>(false);
+	const [visiblePopup, setVisiblePopup] = useState<boolean>(false);
 
-  useEffect(() => {
-    getThongBaoModel();
-  }, [page, limit]);
+	useEffect(() => {
+		getThongBaoModel();
+	}, [page, limit]);
 
-  const clearReadState = async () => {
-    readNotificationModel('ALL');
-    setVisiblePopup(false);
-  };
+	const clearReadState = async () => {
+		readNotificationModel('ALL');
+		setVisiblePopup(false);
+	};
 
-  return (
-    <>
-      <NoticeIcon
-        count={unread}
-        onItemClick={async (item) => {
-          setRecord(item);
-          setVisibleDetail(true);
-          setVisiblePopup(false);
-          if (!item.read) readNotificationModel('ONE', item?._id);
-        }}
-        loading={loading}
-        onClear={() => clearReadState()}
-        clearText="Đánh dấu tất cả là đã đọc"
-        viewMoreText="Tải thêm"
-        onViewMore={() => {
-          if (loading) return;
-          setLimit(limit + 5);
-        }}
-        popupVisible={visiblePopup}
-        clearClose
-        onPopupVisibleChange={(visible) => {
-          setVisiblePopup(visible);
-        }}
-      >
-        <NoticeIcon.Tab
-          tabKey="notification"
-          count={total}
-          list={danhSach}
-          title="Thông báo"
-          emptyText="Bạn đã xem tất cả thông báo"
-          showClear={!!unread}
-          showViewMore={danhSach.length < total}
-        />
-      </NoticeIcon>
+	return (
+		<>
+			<NoticeIcon
+				count={unread}
+				onItemClick={async (item) => {
+					setRecord(item);
+					setVisibleDetail(true);
+					setVisiblePopup(false);
+				}}
+				loading={loading}
+				onClear={() => clearReadState()}
+				clearText='Đánh dấu tất cả là đã đọc'
+				viewMoreText='Tải thêm'
+				onViewMore={() => {
+					if (loading) return;
+					setLimit(limit + 5);
+				}}
+				popupVisible={visiblePopup}
+				clearClose
+				onPopupVisibleChange={(visible) => {
+					setVisiblePopup(visible);
+				}}
+			>
+				<NoticeIcon.Tab
+					tabKey='notification'
+					count={total}
+					list={danhSach}
+					title='Thông báo'
+					emptyText='Bạn đã xem tất cả thông báo'
+					showClear={!!unread}
+					showViewMore={danhSach.length < total}
+				/>
+			</NoticeIcon>
 
-      <Modal
-        width={800}
-        bodyStyle={{ padding: 0 }}
-        destroyOnClose
-        onCancel={() => setVisibleDetail(false)}
-        visible={visibleDetail}
-        okButtonProps={{ hidden: true }}
-        cancelText="Đóng"
-      >
-        <ViewThongBao
-          record={record}
-          afterViewDetail={() => {
-            setVisibleDetail(false);
-            setVisiblePopup(false);
-          }}
-        />
-      </Modal>
-    </>
-  );
+			<Modal
+				width={800}
+				bodyStyle={{ padding: 0 }}
+				destroyOnClose
+				onCancel={() => setVisibleDetail(false)}
+				visible={visibleDetail}
+				okButtonProps={{ hidden: true }}
+				cancelText='Đóng'
+			>
+				<ViewThongBao
+					record={record}
+					afterViewDetail={() => {
+						setVisibleDetail(false);
+						setVisiblePopup(false);
+					}}
+				/>
+			</Modal>
+		</>
+	);
 };
 
 export default NoticeIconView;
